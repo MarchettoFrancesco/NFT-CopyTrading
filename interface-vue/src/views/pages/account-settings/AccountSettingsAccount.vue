d3a901bbd68943c68dba5f9dff81a92cc22d9100
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2022-08-10 13:24:00 +0200

    ipfs upload completed

--- interface-vue/src/views/pages/account-settings/AccountSettingsAccount.vue
+++ interface-vue/src/views/pages/account-settings/AccountSettingsAccount.vue
@@ -1,28 +1,57 @@
 <template>
-  <v-card flat class="pa-3 mt-2">
+  <v-card
+    flat
+    class="pa-3 mt-2"
+  >
     <v-card-text class="d-flex">
-      <v-avatar rounded size="120" class="me-6" @click="$refs.refInputEl.click()" style="cursor:pointer">
-        <v-img :src="nftImage"
-          ><template v-slot:placeholder>
-            <v-row class="fill-height ma-0 primary" align="center" justify="center">
-              <v-icon size="44">{{ icons.mdiCamera }}</v-icon>
+      <v-avatar
+        rounded
+        size="120"
+        class="me-6"
+        style="cursor: pointer"
+        @click="$refs.refInputEl.click()"
+      >
+        <v-img :src="nftImage">
+          <template v-slot:placeholder>
+            <v-row
+              class="fill-height ma-0 primary"
+              align="center"
+              justify="center"
+            >
+              <v-icon size="44">
+                {{ icons.mdiCamera }}
+              </v-icon>
             </v-row>
-          </template></v-img
-        >
+          </template>
+        </v-img>
       </v-avatar>
 
       <!-- upload photo -->
       <div>
-        <v-btn color="primary" class="me-3 mt-5" @click="$refs.refInputEl.click()">
+        <v-btn
+          color="primary"
+          class="me-3 mt-5"
+          @click="$refs.refInputEl.click()"
+        >
           <v-icon class="d-sm-none">
             {{ icons.mdiCloudUploadOutline }}
           </v-icon>
           <span class="d-none d-sm-block">Upload new photo</span>
         </v-btn>
 
-        <input ref="refInputEl" type="file" accept=".jpeg,.png,.jpg,GIF" :hidden="true" @change="previewFiles" />
+        <input
+          ref="refInputEl"
+          type="file"
+          accept=".jpeg,.png,.jpg,GIF"
+          :hidden="true"
+          @change="previewFiles"
+        />
 
-        <v-btn color="error" outlined class="mt-5">
+        <v-btn
+          color="error"
+          outlined
+          class="mt-5"
+        >
           Reset
         </v-btn>
         <p class="text-sm mt-5">
@@ -32,27 +61,71 @@
     </v-card-text>
 
     <v-card-text>
-      <v-form class="multi-col-validation mt-6" @submit.prevent="mint">
+      <v-form
+        class="multi-col-validation mt-6"
+        @submit.prevent="mint"
+      >
         <v-row>
-          <v-col md="6" cols="12">
-            <v-text-field v-model="name" label="Name" dense outlined></v-text-field>
+          <v-col
+            md="6"
+            cols="12"
+          >
+            <v-text-field
+              v-model="name"
+              label="Name"
+              dense
+              outlined
+            ></v-text-field>
           </v-col>
-          <v-col cols="12" md="6">
-            <v-text-field v-model="amount" label="Amout" type="number" step="1" dense outlined></v-text-field>
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-text-field
+              v-model="amount"
+              label="Amout"
+              type="number"
+              step="1"
+              dense
+              outlined
+            ></v-text-field>
           </v-col>
           <v-col cols="12">
-            <v-textarea v-model="description" label="Description" dense outlined></v-textarea>
+            <v-textarea
+              v-model="description"
+              label="Description"
+              dense
+              outlined
+            ></v-textarea>
           </v-col>
 
-          <v-col cols="12" md="6">
-            <v-text-field v-model="symbol" dense label="Symbol" outlined></v-text-field>
+          <v-col
+            cols="12"
+            md="6"
+          >
+            <v-text-field
+              v-model="symbol"
+              dense
+              label="Symbol"
+              outlined
+            ></v-text-field>
           </v-col>
 
           <v-col cols="12">
-            <v-btn color="primary" class="me-3 mt-4" @click="mint">
+            <v-btn
+              color="primary"
+              class="me-3 mt-4"
+              @click="mint"
+            >
               Mint
             </v-btn>
-            <v-btn color="secondary" outlined class="mt-4" type="reset" @click.prevent="resetForm">
+            <v-btn
+              color="secondary"
+              outlined
+              class="mt-4"
+              type="reset"
+              @click.prevent="resetForm"
+            >
               Cancel
             </v-btn>
           </v-col>
@@ -65,14 +138,13 @@
 <script>
 import { mdiAlertOutline, mdiCloudUploadOutline, mdiCamera } from '@mdi/js'
 import { ref } from '@vue/composition-api'
-import { NFTStorage, File } from 'nft.storage'
+import { NFTStorage, File } from 'nft.storage/dist/bundle.esm.min.js'
 
-const NFT_STORAGE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJlODQ1QmE3OTZEMmU5YTRGNzY5YjFhMjEwNjZDQzQwNGIzN2Q1N0MiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MDEyNDMwMDEzMiwibmFtZSI6ImFwaGVsaW9zIn0.546qPlU6ENvYHwcollzJlA-_kfIuZxT8OAm7QfgyWxs'
+const NFT_STORAGE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJlODQ1QmE3OTZEMmU5YTRGNzY5YjFhMjEwNjZDQzQwNGIzN2Q1N0MiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MDEyNDMwMDEzMiwibmFtZSI6ImFwaGVsaW9zIn0.546qPlU6ENvYHwcollzJlA-_kfIuZxT8OAm7QfgyWxs'
 
 const nftstorage = new NFTStorage({ token: NFT_STORAGE_KEY })
 
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJlODQ1QmE3OTZEMmU5YTRGNzY5YjFhMjEwNjZDQzQwNGIzN2Q1N0MiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MDEyNDMwMDEzMiwibmFtZSI6ImFwaGVsaW9zIn0.546qPlU6ENvYHwcollzJlA-_kfIuZxT8OAm7QfgyWxs
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJlODQ1QmE3OTZEMmU5YTRGNzY5YjFhMjEwNjZDQzQwNGIzN2Q1N0MiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MDEyNDMwMDEzMiwibmFtZSI6ImFwaGVsaW9zIn0.546qPlU6ENvYHwcollzJlA-_kfIuZxT8OAm7QfgyWxs
 // aphelios
 export default {
   props: {
@@ -81,27 +153,35 @@ export default {
       default: () => {},
     },
   },
+  data() {
+    return {
+      nftImage: '',
+      name: '',
+      description: '',
+      amount: 1,
+      symbol: '',
+      filesContent: null,
+      files: null,
+    }
+  },
   methods: {
     previewFiles(event) {
-      console.log(event.target.files[0])
       const nftImage = URL.createObjectURL(event.target.files[0])
       this.filesContent = event.target.files[0]
+      console.log(this.filesContent)
       this.nftImage = nftImage
     },
     async mint() {
+      console.log(this.filesContent)
       const metadata = await nftstorage.store({
         name: this.name,
         description: this.description,
         decimals: 0,
         symbol: this.symbol,
-        image: new File([this.filesContent[0].content], this.filesContent[0].name, {
-          type: 'image/' + this.filesContent[0].name.split('.')[1],
-        }),
+        image: this.filesContent,
       })
+      console.log(metadata)
     },
-  },
-  data() {
-    return { nftImage: '', name: '', description: '', amount: 1, symbol: '', filesContent: null }
   },
   setup(props) {
     const status = ['Active', 'Inactive', 'Pending', 'Closed']
